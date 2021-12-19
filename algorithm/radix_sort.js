f46cc4791b5971ce615a1cd27db5e225a3f54165
wonsss <bigdatajang@gmail.com>
2021-12-19 22:36:52 +0900

    자료구조 스터디

--- algorithm/radix_sort.js
+++ algorithm/radix_sort.js
@@ -15,25 +15,33 @@ function mostDigits(nums) {
   return maxDigits;
 }
 
-function radixSort(nums){
-    let maxDigitCount = mostDigits(nums);
-    for(let k = 0; k < maxDigitCount; k++){
-        let digitBuckets = Array.from({length: 10}, () => []);
-        for(let i = 0; i < nums.length; i++){
-            let digit = getDigit(nums[i],k);
-            digitBuckets[digit].push(nums[i]);
-        }
-        nums = [].concat(...digitBuckets);
+function radixSort(nums) {
+  let maxDigitCount = mostDigits(nums);
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < nums.length; i++) {
+      let digit = getDigit(nums[i], k);
+      digitBuckets[digit].push(nums[i]);
     }
-    return nums;
+    nums = [].concat(...digitBuckets);
+  }
+  return nums;
 }
 
-radixSort([23,345,5467,12,2345,9852])
+radixSort([23, 345, 5467, 12, 2345, 9852]);
 
+function radixSort(nums) {
+  let maxDigitCount = mostDigits(nums); // 4
+  for (let k = 0; k < maxDigitCount; k++) {
+    let digitBuckets = Array.from({ length: 10 }, () => []);
+    for (let i = 0; i < nums.length; i++) {
+      let digit = getDigit(nums[i], k);
+      digitBuckets[digit].push(nums[i]); // nums 배열의 i번째 수를 그 숫자의 k번째(1의 자리는 0부터) 자릿수의 순서에 있는 그릇에 넣는다.
+    }
+    nums = [].concat(...digitBuckets); // concat 메서드와 spread 연산자를 사용하여 한 배열 안에 있는 여러 배열들을 하나의 배열로 합친다.
+  }
+  return nums;
+}
 
-
-
-
-
-
-
+console.log(radixSort([23, 345, 5467, 12, 2345, 9852]));
+// [ 12, 23, 345, 2345, 5467, 9852 ]
--- dataStructure/Singly_Linked_List_Push.js
+++ dataStructure/Singly_Linked_List_Push.js
@@ -1,30 +1,42 @@
-class Node{
-    constructor(val){
-        this.val = val;
-        this.next = null;
-    }
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
 }
 
-class SinglyLinkedList{
-    constructor(){
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(val) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
     }
-    push(val){
-        var newNode = new Node(val);
-        if(!this.head){
-            this.head = newNode;
-            this.tail = this.head;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-        this.length++;
-        return this;
-    }
+    this.length++;
+    return this;
+  }
 }
 
-var list = new SinglyLinkedList()
-// list.push("HELLO")
-// list.push("GOODBYE")
+const list = new SinglyLinkedList();
+list.push('HELLO');
+list.push('MARCO');
+list.push('GOODBYE');
+
+console.log(list);
+
+/* 
+SinglyLinkedList {
+  head: Node { val: 'HELLO', next: Node { val: 'MARCO', next: [Node] } },
+  tail: Node { val: 'GOODBYE', next: null },
+  length: 3
+}
+ */
